b27864776da7006e33b2c3c1c20112463242014f
Yuna0987 <kangxunyun@gmail.com>
2025-10-03 17:14:43 +0200

    Update run.py to plot sine wave instead of printing samples

--- run.py
+++ run.py
@@ -1,12 +1,33 @@
-from signals import generate_sine_wave
-
-
-frequency = 5      
+from signals import generate_sine_wave,generate_unit_step
+imfrequency = 5      
 duration = 2       
-sample_rate = 100  
-
-
+sample_rate = 100 port matlplot .pyplot as plt
 wave = generate_sine_wave(frequency, duration, sample_rate)
+t,y = generate_sine_wave(5,2,100)
+plt.plot(t,y,label="Original sine wave")
+
+t_shifted = t+0.5
+plt.plot(t_shifted, y, label="Time shifted (+0.5s)")
+t_scaled=t*0.5
+plt.plot(t_scaled, y, label="Time scaled (0.5x)"
+         
+t2,u= generate_unit_step(2,100, step_time=1)
+plt.plot(t2, u, label="Unit step (at t=1s)")
+         y_sum=y+u
+         plt.plot(t,y_sum, label ="sine + unit step")
+         plt.xlabel("Time(s)")
+plt.ylabel("Amplitude")
+plt.titel ("Signal transformations")
+plt.legend()
+plt.grid(True)
+plt.show()
+       
+
+
+ 
+
+
+
 
 
 print("First 10 samples:", wave[:10])
