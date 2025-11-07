c2e91f03190747a6e58ae192250e64810a2235c1
Yuna0987 <kangxunyun@gmail.com>
2025-11-07 16:12:17 +0100

    Update README.py

--- README.py
+++ README.py
@@ -1,15 +1,1 @@
 xunyun kang (6311091)
-Signal Generator and Modifier
-
-This project contains scripts to generate, visualize, and manipulate basic signals.
-
-
-
-To run the project, you should have Python and the following packages:
-
-- numpy
-- matplotlib
-
-Then execute: 
-python run.py
-print (readme_content)
